23f5e515ad466c21252f0717e46b548862195785
Viral Savani <viralsavani@gmail.com>
2015-01-14 13:16:35 -0800

    V2_Inventory :: Show products with brand name (Done) No @QueryParam mentioned error tackled (Done)

--- src/com/rest/inventory/V2_Inventory.java
+++ src/com/rest/inventory/V2_Inventory.java
@@ -1,5 +1,8 @@
 package com.rest.inventory;
 
+import com.rest.Dao.Schema;
+import com.sun.jersey.api.container.httpserver.HttpServerFactory;
+import com.sun.net.httpserver.HttpServer;
 import org.codehaus.jettison.json.JSONArray;
 
 import javax.ws.rs.GET;
@@ -8,9 +11,12 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.IOException;
 
 /**
  * Created by VIRAL on 1/14/2015.
+ * Use of @QueryParam
+ * It will look of anything after "?"
  */
 @Path("/v2/inventory/*")
 public class V2_Inventory {
@@ -22,8 +28,13 @@ public class V2_Inventory {
         JSONArray jsonArray = new JSONArray();
 
         try{
+            if(brand == null || brand.equals("")){
+                return Response.status(400).entity("Please enter a brand name as QueryParameter").build();
+            }
 
-
+            Schema newSchema = new Schema();
+            jsonArray = newSchema.queryToReturnBrandParts(brand);
+            returnString = jsonArray.toString();
 
         }catch (Exception e){
             e.printStackTrace();
@@ -31,6 +42,19 @@ public class V2_Inventory {
         }
 
         return Response.ok(returnString).build();
+    }
+
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServerFactory.create("http://localhost:9998/");
+        server.start();
+
+        System.out.println("Server running");
+        System.out.println("Visit: http://localhost:9998/");
+        System.out.println("Hit return to stop...");
+        System.in.read();
+        System.out.println("Stopping server");
+        server.stop(0);
+        System.out.println("Server stopped");
     }
 
 }
